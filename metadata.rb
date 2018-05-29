54fed96ee68a1dc231d239f7bb871fb0594e56e8
Ethan Peterson <ethanpeterson@gmail.com>
2018-05-29 14:25:23 -0500

    added ability to read AWS SSL Settings from OpsWorks

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'Apache-2.0'
 description 'Installs & configures serverbase - a MERN environment'
 long_description 'Installs & configures serverbase, which consists of an nginx front end backed by node.js and mongoDb on one instance'
-version '0.1.1'
+version '0.1.2'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/ethanpeterson/serverbase/issues'
